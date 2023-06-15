8f8f59efbf12eeda2a82af2c52bc391721d11faa
Maksim El'kin <LiveParadox@vk.com>
2023-06-15 23:17:37 +0300

    I changed the interface, tasks are displayed correctly. Added three options for each task: Delete, Edit, and Complete. Implemented hover effect on the selected task. Resolved the issue with long task text by allowing word wrapping. Updated the modal window interface: added a calendar picker, added an additional input for date, changed the date format, and added a close button,removed the unnecessary close button from the modal window.

--- App.tsx
+++ App.tsx
@@ -1,27 +1,89 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, StatusBar } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+  TouchableHighlight,
+} from "react-native";
 import ModalWindow from "./components/Modal/ModalWindow";
 import ButtonAddQvest from "./components/UI/Button/ButtonAddQvest";
+import { AntDesign, Entypo } from "@expo/vector-icons";
 
 type Task = {
   name: string;
   date: string;
+  completed: boolean;
 };
 
 const App = () => {
-  const [taskList, setTaskList] = useState<{ name: string; date: string }[]>(
-    []
-  );
+  const [taskList, setTaskList] = useState<Task[]>([]);
   const [modalVisible, setModalVisible] = useState(false);
+  const [selectedTaskIndex, setSelectedTaskIndex] = useState<number | null>(
+    null
+  );
+  const [taskName, setTaskName] = useState("");
+  const [taskDate, setTaskDate] = useState("");
 
   const handleAddTask = (taskName: string, taskDate: string) => {
-    setModalVisible(true);
-    setTaskList([...taskList, { name: taskName, date: taskDate }]);
+    setModalVisible(false);
+
+    if (selectedTaskIndex !== null) {
+      const updatedTaskList = [...taskList];
+      updatedTaskList[selectedTaskIndex] = {
+        name: taskName,
+        date: taskDate,
+        completed: updatedTaskList[selectedTaskIndex].completed,
+      };
+      setTaskList(updatedTaskList);
+    } else {
+      const task: Task = { name: taskName, date: taskDate, completed: false };
+      setTaskList([...taskList, task]);
+    }
+
+    setSelectedTaskIndex(null);
+    setTaskName("");
+    setTaskDate("");
   };
 
-  const handleCloseModalWindow = () => {
-    setModalVisible(false);
+  const handleTaskPress = (index: number) => {
+    setSelectedTaskIndex(index);
   };
+
+  const handleDeleteTask = (index: number) => {
+    const updatedTaskList = [...taskList];
+    updatedTaskList.splice(index, 1);
+    setTaskList(updatedTaskList);
+    setSelectedTaskIndex(null);
+  };
+
+  const handleCompleteTask = (index: number) => {
+    const updatedTaskList = [...taskList];
+    if (index >= 0 && index < updatedTaskList.length) {
+      const task = updatedTaskList[index];
+      if (task && !task.completed) {
+        task.completed = true;
+        setTaskList(updatedTaskList);
+      }
+    }
+  };
+
+  const handleEditTask = (index: number) => {
+    setModalVisible(true);
+    setSelectedTaskIndex(index);
+
+    const selectedTask = taskList[index];
+    setTaskName(selectedTask.name);
+    setTaskDate(selectedTask.date);
+  };
+
+  useEffect(() => {
+    setTaskList([
+      { name: "Задача 1", date: "16-06-2023", completed: false },
+      { name: "Задача 2", date: "17-06-2023", completed: false },
+    ]);
+  }, []);
 
   return (
     <View style={styles.content}>
@@ -31,7 +93,7 @@ const App = () => {
       </View>
       <View>
         <ButtonAddQvest
-          onPress={handleAddTask}
+          onPress={() => setModalVisible(true)}
           title="Добавить задачу"
           buttonColor="#rgb(255,69,0)"
         />
@@ -39,12 +101,51 @@ const App = () => {
       <ModalWindow
         modalVisible={modalVisible}
         setModalVisible={setModalVisible}
-        handleAddTask={handleCloseModalWindow}
+        handleAddTask={handleAddTask}
         taskList={taskList}
+        task={selectedTaskIndex !== null ? taskList[selectedTaskIndex] : null}
+        setTaskName={setTaskName}
+        setTaskDate={setTaskDate}
       />
       <View style={styles.taskList}>
         {taskList.map((task, index) => (
-          <Text key={index}>{task.name}</Text>
+          <TouchableHighlight
+            key={index}
+            onPress={() => handleTaskPress(index)}
+            style={[
+              styles.taskItem,
+              selectedTaskIndex === index && { backgroundColor: "salmon" },
+              task.completed && { backgroundColor: "#00CC00" },
+            ]}
+          >
+            <View style={styles.taskContent}>
+              <Text style={styles.taskText}>
+                {index + 1}. {task.name} - {task.date}
+              </Text>
+              {!task.completed && selectedTaskIndex === index && (
+                <>
+                  <TouchableOpacity
+                    style={styles.iconButton}
+                    onPress={() => handleCompleteTask(index)}
+                  >
+                    <Entypo name="check" size={24} color="black" />
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={styles.iconButton}
+                    onPress={() => handleEditTask(index)}
+                  >
+                    <Entypo name="edit" size={24} color="black" />
+                  </TouchableOpacity>
+                </>
+              )}
+              <TouchableOpacity
+                style={styles.iconButton}
+                onPress={() => handleDeleteTask(index)}
+              >
+                <AntDesign name="delete" size={18} color="black" />
+              </TouchableOpacity>
+            </View>
+          </TouchableHighlight>
         ))}
       </View>
     </View>
@@ -65,7 +166,31 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   taskList: {
-    marginTop: 20,
+    marginTop: 40,
+    alignSelf: "center",
+  },
+  taskItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+    padding: 10,
+    borderRadius: 8,
+    backgroundColor: "#f2f2f2",
+    width: "100%",
+  },
+  taskContent: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: 300,
+  },
+  taskText: {
+    fontSize: 18,
+    color: "#606060",
+    flex: 1,
+  },
+  iconButton: {
+    marginLeft: 10,
   },
 });
 
--- components/Modal/ModalWindow.tsx
+++ components/Modal/ModalWindow.tsx
@@ -15,58 +15,117 @@ type ModalWindowProps = {
   setModalVisible: (visible: boolean) => void;
   handleAddTask: (taskName: string, taskDate: string) => void;
   taskList: Task[];
+  task?: Task | null;
+  setTaskName: (name: string) => void;
+  setTaskDate: (date: string) => void;
 };
+
 type Task = {
   name: string;
   date: string;
 };
+
 const ModalWindow: React.FC<ModalWindowProps> = ({
   modalVisible,
   setModalVisible,
   handleAddTask,
-  taskList:task
+  taskList,
 }) => {
   const [taskName, setTaskName] = useState("");
   const [taskDate, setTaskDate] = useState("");
-  const [taskList, setTaskList] = useState([]);
+  const [taskNameError, setTaskNameError] = useState("");
+  const [taskDateError, setTaskDateError] = useState("");
+  const [showCalendar, setShowCalendar] = useState(false);
 
   const handleConfirm = () => {
+    if (!validateFields()) {
+      return;
+    }
     handleAddTask(taskName, taskDate);
     setTaskName("");
     setTaskDate("");
     setModalVisible(false);
-    setTaskList([...taskList, { name: taskName, date: taskDate }]);
   };
 
   const handleClose = () => {
     setModalVisible(false);
   };
 
+  const handleToggleCalendar = () => {
+    setShowCalendar(!showCalendar);
+  };
+
   const handleDayPress = (date: any) => {
     setTaskDate(date.dateString);
+    setShowCalendar(false);
+  };
+
+  const validateFields = () => {
+    let valid = true;
+    setTaskNameError("");
+    setTaskDateError("");
+
+    if (taskName.trim() === "") {
+      setTaskNameError("Пожалуйста, введите название задачи");
+      valid = false;
+    }
+
+    if (taskDate.trim() === "") {
+      setTaskDateError("Пожалуйста, выберите дату");
+      valid = false;
+    }
+
+    return valid;
   };
 
   return (
     <Modal visible={modalVisible} animationType="slide" transparent>
       <View style={styles.modalContainer}>
         <View style={styles.modalContent}>
-          <Text style={styles.modalTitle}>Добавить задачу</Text>
           <TextInput
             style={styles.input}
             placeholder="Введите название задачи"
             value={taskName}
             onChangeText={(text) => setTaskName(text)}
           />
-          <Calendar onDayPress={handleDayPress} />
-          <TouchableOpacity onPress={handleClose} style={styles.closeButton}>
-            <Feather name="x" size={24} color="black" />
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={handleConfirm}
-            style={styles.confirmButton}
-          >
-            <Text style={styles.buttonText}>Подтвердить</Text>
-          </TouchableOpacity>
+          <View style={styles.errorContainer}>
+            {taskNameError ? (
+              <Text style={styles.errorText}>{taskNameError}</Text>
+            ) : null}
+          </View>
+          <View style={styles.dateContainer}>
+            <TextInput
+              style={styles.dateInput}
+              placeholder="Введите дату"
+              value={taskDate}
+              onChangeText={(text) => setTaskDate(text)}
+            />
+            <TouchableOpacity
+              style={styles.calendarButton}
+              onPress={handleToggleCalendar}
+            >
+              <Feather name="calendar" size={24} color="gray" />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.errorContainer}>
+            {taskDateError ? (
+              <Text style={styles.errorText}>{taskDateError}</Text>
+            ) : null}
+          </View>
+          {showCalendar && (
+            <Calendar onDayPress={handleDayPress} hideExtraDays />
+          )}
+          <View style={styles.btnHandle}>
+            <TouchableOpacity onPress={handleClose} style={styles.closeButton}>
+              <Text style={styles.buttonText}>Закрыть</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={handleConfirm}
+              style={styles.confirmButton}
+            >
+              <Text style={styles.buttonText}>Подтвердить</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     </Modal>
@@ -86,11 +145,6 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     width: "80%",
   },
-  modalTitle: {
-    fontSize: 18,
-    alignSelf: "center",
-    marginBottom: 20,
-  },
   input: {
     height: 40,
     borderColor: "gray",
@@ -98,21 +152,55 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     paddingHorizontal: 10,
   },
+  dateContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  dateInput: {
+    flex: 1,
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    paddingHorizontal: 10,
+  },
+  calendarButton: {
+    marginLeft: 10,
+  },
   closeButton: {
-    position: "absolute",
-    top: 10,
-    right: 10,
+    flex: 1,
+    backgroundColor: "lightgray",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    alignSelf: "center",
+    marginRight: 10,
   },
   confirmButton: {
+    flex: 1,
     backgroundColor: "lightblue",
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 10,
     alignSelf: "center",
+    marginLeft: 10,
   },
   buttonText: {
     fontSize: 16,
     fontWeight: "bold",
+    textAlign: "center",
+  },
+  btnHandle: {
+    flexDirection: "row",
+    marginTop: 10,
+  },
+  errorContainer: {
+    marginTop: 10,
+  },
+  errorText: {
+    color: "red",
+    fontSize: 12,
+    marginTop: -20,
   },
 });
 
